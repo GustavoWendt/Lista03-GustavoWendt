fbe61f30f5b4ba369cdce372e3a9b7fe677db610
GustavoWendt <gustavowendt14@gmail.com>
2025-02-11 22:15:48 -0300

    Atividades

---
+++ Ex04-GustavoWendt.py
@@ -0,0 +1,9 @@
+#Peça ao usuário para inserir sua cor favorita. Se ele digitar "vermelho", "VERMELHO" ou "Vermelho" exibem a mensagem "Eu também gosto de vermelho", caso contrário, exibem a mensagem "Eu não gosto de [cor], eu prefiro vermelho".
+cor=input("Digite qual a sua cor favorita: ")
+cor1=cor.title()
+if cor1 == 'Vermelho':
+    print("É minha cor favorita também!")
+    print("Gustavo Wendt")
+else:
+    print("Eu não gosto desta", cor1,"cor:")
+    print("Gustavo Wendt")
